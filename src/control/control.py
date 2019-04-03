1b9b2f47c4109efcfe209d5bd14da5b34b3545ae
thinkofher <piaskupiasku@gmail.com>
2019-04-03 14:25:25 +0200

    created ui terminal interface for robot controlling

---
+++ src/control/control.py
@@ -0,0 +1,38 @@
+import curses
+from .curses import msgClicked, curEndSession
+
+
+class ScreenController:
+
+    _default_distance = 0.030
+
+    def __init__(self, screen, driver):
+        self.screen = screen
+        self.driver = driver
+        self._distance = self._default_distance
+
+    def set_distance(self, distance):
+        self._distance = distance
+
+    def run(self):
+        try:
+            while True:
+                char = self.screen.getch()
+                if char == ord('q'):
+                    break
+                elif char == curses.KEY_UP:
+                    msgClicked(self.screen, 'UP')
+                    self.driver.forward(self.distance)
+                elif char == curses.KEY_DOWN:
+                    msgClicked(self.screen, 'DOWN')
+                    self.driver.backward(self.distance)
+                elif char == curses.KEY_LEFT:
+                    msgClicked(self.screen, 'LEFT')
+                    self.driver.turnLeftForward(self.distance)
+                elif char == curses.KEY_RIGHT:
+                    msgClicked(self.screen, 'RIGHT')
+                    self.driver.turnRightForward(self.distance)
+                else:
+                    msgClicked(self.screen, "")
+        finally:
+            curEndSession(self.screen)
---
+++ src/control/curses.py
@@ -0,0 +1,31 @@
+import curses
+
+
+def curNewScreen():
+    screen = curses.initscr()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    curses.noecho()
+    curses.cbreak()
+    screen.keypad(True)
+    return screen
+
+
+def msgClicked(screen, button):
+    if len(button) == 0:
+        msgString = "CANT RECOGNIZE!\n"
+    else:
+        msgString = f'Clicked: {button}!\n'
+    try:
+        screen.addstr(msgString,
+                      curses.A_REVERSE)
+        screen.refresh()
+    except curses.error:
+        screen.clear()
+
+
+def curEndSession(screen):
+    curses.nocbreak()
+    screen.keypad(0)
+    curses.echo()
+    curses.endwin()
